b8acf2cd982df57376bf841804dcb6b5edf009fe
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-08-21 11:23:57 +0100

    Add `inspect` command (#30)

--- packages/impressions-cli/src/command/mod.rs
+++ packages/impressions-cli/src/command/mod.rs
@@ -1,12 +1,17 @@
 mod analyse;
+mod inspect;
 
 use clap::Subcommand;
 
 use self::analyse::Analyse;
+use self::inspect::Inspect;
 
 #[derive(Subcommand)]
 pub enum Command {
     /// Analyse a binary.
     #[command(visible_alias = "analyze")]
     Analyse(Analyse),
+
+    /// Inspect a binary analysis.
+    Inspect(Inspect),
 }
