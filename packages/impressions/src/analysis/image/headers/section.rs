f10ba55939bcb3e4eb62d3701389b99f586f0700
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-08-22 10:48:53 +0100

    Add section characteristics analysis

--- packages/impressions/src/analysis/image/headers/section.rs
+++ packages/impressions/src/analysis/image/headers/section.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Debug};
+
 use bytes::Buf;
 use serde::{Deserialize, Serialize};
 
@@ -60,8 +62,8 @@ pub struct SectionHeader {
     /// The number of line-number entries for the section.
     number_of_linenumbers: u16,
 
-    /// The characteristics of the image.
-    characteristics: u32,
+    /// The characteristics of the section.
+    characteristics: SectionCharacteristics,
 }
 
 impl SectionHeader {
@@ -94,6 +96,11 @@ impl SectionHeader {
     pub fn file_size(&self) -> usize {
         self.size_of_raw_data as usize
     }
+
+    /// Gets the section characteristics.
+    pub fn characteristics(&self) -> SectionCharacteristics {
+        self.characteristics
+    }
 }
 
 impl Parse for SectionHeader {
@@ -110,7 +117,121 @@ impl Parse for SectionHeader {
             pointer_to_linenumbers: buffer.try_get_u32_le()?,
             number_of_relocations: buffer.try_get_u16_le()?,
             number_of_linenumbers: buffer.try_get_u16_le()?,
-            characteristics: buffer.try_get_u32_le()?,
+            characteristics: SectionCharacteristics::parse(&mut buffer)?,
         })
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(transparent)]
+#[repr(transparent)]
+pub struct SectionCharacteristics(u32);
+
+impl SectionCharacteristics {
+    /// The section contains executable code.
+    const CNT_CODE: u32 = 0x00000020;
+
+    /// The section contains initialized data.
+    const CNT_INITIALIZED_DATA: u32 = 0x00000040;
+
+    /// The section contains uninitialized data.
+    const CNT_UNINITIALIZED_DATA: u32 = 0x00000080;
+
+    /// The section can be discarded as needed.
+    const MEM_DISCARDABLE: u32 = 0x02000000;
+
+    /// The section cannot be cached.
+    const MEM_NOT_CACHED: u32 = 0x04000000;
+
+    /// The section is not pageable.
+    const MEM_NOT_PAGED: u32 = 0x08000000;
+
+    /// The section can be shared in memory.
+    const MEM_SHARED: u32 = 0x10000000;
+
+    /// The section can be executed as code.
+    const MEM_EXECUTE: u32 = 0x20000000;
+
+    /// The section can be read.
+    const MEM_READ: u32 = 0x40000000;
+
+    /// The section can be written to.
+    const MEM_WRITE: u32 = 0x80000000;
+}
+
+impl SectionCharacteristics {
+    /// Checks if the section is readable.
+    pub const fn read(self) -> bool {
+        self.0 & Self::MEM_READ != 0
+    }
+
+    /// Checks if the section is writable.
+    pub const fn write(self) -> bool {
+        self.0 & Self::MEM_WRITE != 0
+    }
+
+    /// Checks if the section is executable.
+    pub const fn execute(self) -> bool {
+        self.0 & Self::MEM_EXECUTE != 0
+    }
+
+    /// Checks if the section is shareable.
+    pub const fn share(self) -> bool {
+        self.0 & Self::MEM_SHARED != 0
+    }
+
+    /// Checks if the section is pageable.
+    pub const fn page(self) -> bool {
+        self.0 & Self::MEM_NOT_PAGED == 0
+    }
+
+    /// Checks if the section is cacheable.
+    pub const fn cache(self) -> bool {
+        self.0 & Self::MEM_NOT_CACHED == 0
+    }
+
+    /// Checks if the section is discardable.
+    pub const fn discard(self) -> bool {
+        self.0 & Self::MEM_DISCARDABLE != 0
+    }
+
+    /// Checks if the section contains initialised data.
+    pub const fn initialised(self) -> bool {
+        self.0 & Self::CNT_INITIALIZED_DATA != 0
+    }
+
+    /// Checks if the section contains uninitialised data.
+    pub const fn uninitialised(self) -> bool {
+        self.0 & Self::CNT_UNINITIALIZED_DATA != 0
+    }
+
+    /// Checks if the section contains code.
+    pub const fn code(self) -> bool {
+        self.0 & Self::CNT_CODE != 0
+    }
+}
+
+impl Parse for SectionCharacteristics {
+    type Error = Error;
+
+    fn parse(mut buffer: impl Buf) -> Result<Self, Self::Error> {
+        Ok(Self(buffer.try_get_u32_le()?))
+    }
+}
+
+impl Debug for SectionCharacteristics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Characteristics")
+            .field("read", &self.read())
+            .field("write", &self.write())
+            .field("execute", &self.execute())
+            .field("share", &self.share())
+            .field("page", &self.page())
+            .field("cache", &self.cache())
+            .field("discard", &self.discard())
+            .field("initialised", &self.initialised())
+            .field("uninitialised", &self.uninitialised())
+            .field("code", &self.code())
+            .finish()
     }
 }
