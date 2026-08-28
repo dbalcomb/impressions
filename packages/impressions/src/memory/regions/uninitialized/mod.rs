3fc99ff3b4b01946b01b9fb9157ffa200dadc216
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-08-28 09:14:49 +0100

    Add `Uninitialized` region

---
+++ packages/impressions/src/memory/regions/uninitialized/mod.rs
@@ -0,0 +1,30 @@
+//! A region of uninitialized memory.
+
+use serde::{Deserialize, Serialize};
+
+use crate::analysis::Completion;
+use crate::memory::Extent;
+
+/// A region of uninitialized memory.
+#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Uninitialized(u64);
+
+impl Uninitialized {
+    /// Constructs a new uninitialized region.
+    pub const fn new(size: u64) -> Self {
+        Self(size)
+    }
+}
+
+impl Extent for Uninitialized {
+    fn size(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Completion for Uninitialized {
+    fn identified(&self) -> u64 {
+        0
+    }
+}
