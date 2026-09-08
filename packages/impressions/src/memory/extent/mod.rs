86e58d244b868b44d7f8a05d838720b7d04f3f01
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-09-08 20:54:14 +0100

    Add `address_space` method to `Extent`

--- packages/impressions/src/memory/extent/mod.rs
+++ packages/impressions/src/memory/extent/mod.rs
@@ -6,8 +6,15 @@ mod size;
 pub use self::error::Error;
 pub use self::size::Size;
 
+use super::address::AddressSpace;
+
 /// Defines the size of a memory region.
 pub trait Extent {
     /// Gets the size of the region.
     fn size(&self) -> Size;
+
+    /// Gets the address space of the region.
+    fn address_space(&self) -> AddressSpace {
+        self.size().to_address_space()
+    }
 }
